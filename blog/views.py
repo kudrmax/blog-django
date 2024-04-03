4dac8449ffd094b7e9f552fba59582b560fa0c8d
kudr.max <mdkudryashov@edu.hse.ru>
2024-04-03 16:28:44 +0300

    Added "about" page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
     return HttpResponse('<h1>My Home Page</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>My About Page</h1>')
