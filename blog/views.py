70142e477682304f304ad4b901d6b5a5df407b43
kudr.max <mdkudryashov@edu.hse.ru>
2024-04-03 19:35:49 +0300

    Made first migration and added superuser

--- blog/views.py
+++ blog/views.py
@@ -3,29 +3,30 @@ from django.http import HttpResponse
 
 posts = [
     {
-        'author': 'Vika',
-        'title': 'What if all your friends got married',
-        'content': "I guess it's time to start a support group for 'Single Survivors Anonymous'!",
+        'author': 'Nikita Tkachenko',
+        'title': "Why are trains such great conversationalists?",
+        'content': "Because they can't go in another direction of the conversation!",
+        'date_posted': 'April 2, 2024',
+    },
+    {
+        'author': 'Vika Pugach',
+        'title': 'What if all your friends got married?',
+        'content': "Time to change your group chat name to 'The Married and the Restless'!",
         'date_posted': 'April 3, 2024',
     },
     {
-        'author': 'Rustam',
+        'author': 'Rustam Radjabov',
         'title': "Why should you think in several programming languages at the same time?",
         'content': "So you can argue with yourself in C++, Python, and Kotlin!",
         'date_posted': 'April 1, 2024',
-    },
-    {
-        'author': 'Nikita',
-        'title': "Why is it cool to be a devops?",
-        'content': "Because they're the masters of making chaos look like controlled chaos!",
-        'date_posted': 'April 2, 2024',
     },
 ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': posts,
+        'title': 'Home Page',
     }
     return render(request, template_name='blog/home.html', context=context)
 
