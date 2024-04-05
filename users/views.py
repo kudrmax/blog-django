ef04d636660dde30ea45e1d257e38fa6b45c92a7
kudr.max <mdkudryashov@edu.hse.ru>
2024-04-05 17:58:13 +0300

    Added profile and fixed navbar

--- users/views.py
+++ users/views.py
@@ -14,3 +14,7 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, template_name='users/register.html', context={'form': form})
+
+
+def profile(request):
+    return render(request, template_name='users/profile.html')
